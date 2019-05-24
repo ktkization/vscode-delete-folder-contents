0e504dcbd31e907fbaa9b9442ba7e5fd7691f5a4
Kenneth Thuku <thuku.ken@live.com>
2019-05-24 20:47:15 +0300

    Add functionality for deleting contents of a folder

--- extension.js
+++ extension.js
@@ -1,20 +1,49 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
+const fs = require("fs-extra");
+const path = require("path");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('extension.clearFolderContents', function () {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    "extension.deleteFolderContents",
+    async function(uri) {
+      const targetPath = uri.fsPath;
+      const targetFolder = vscode.workspace.asRelativePath(uri);
 
-	context.subscriptions.push(disposable);
+      vscode.window
+        .withProgress(
+          {
+            location: vscode.ProgressLocation.Notification,
+            title: `Deleting contents in ${targetFolder} folder`,
+            cancellable: true
+          },
+          (progress, token) => {
+            fs.emptyDirSync(targetPath);
+            vscode.commands.executeCommand(
+              "workbench.files.action.refreshFilesExplorer"
+            );
+            return Promise.resolve();
+          }
+        )
+        .then(() => {
+          setTimeout(() => {
+            vscode.window.showInformationMessage(
+              `Succesfully deleted contents in ${targetFolder} folder`
+            );
+          }, 1000);
+        });
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
